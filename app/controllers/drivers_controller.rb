0d9ee6d115909ac085f4a3a3ce96d4bb7e99d712
= <=>
2023-09-23 00:16:04 +0530

    Adding Caching for available_driver

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -5,8 +5,28 @@ require 'json'
 # This class manages the actions related to drivers
 class DriversController < ApplicationController
   def available_driver
-    drivers = Driver.all
-    render json: drivers
+    # drivers = Driver.all
+
+    # fresh_when drivers
+
+    cache_key = "drivers_index/#{Driver.maximum(:updated_at).to_i}"
+
+    # Try to read the cached data
+    @drivers = Rails.cache.fetch(cache_key) do
+      # If the cached data is not available or is outdated, fetch the data from the server
+      Driver.available
+    end
+
+    # Compare the timestamp of the cached data with the timestamp of the data from the server
+    if @drivers.maximum(:updated_at).to_i > cache_key.split('/').last.to_i
+      # If the data from the server is newer than the cached data, expire the cache and fetch the data from the server
+      Rails.cache.delete(cache_key)
+      @drivers = Driver.available
+    end
+
+    # Set the cache expiration time
+    expires_in 10.minutes, public: true
+    render json: @drivers
   end
 
   def not_available_driver
